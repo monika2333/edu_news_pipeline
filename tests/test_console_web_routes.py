8700b3ef8f8a82861d5854198fa66292086cff6c
Monica2333 <1013766976@qq.com>
2026-07-28 11:57:23 +0800

    更新补丁

--- tests/test_console_web_routes.py
+++ tests/test_console_web_routes.py
@@ -479,6 +479,10 @@ def test_duty_summary_collapses_shift_panel_by_default(
     assert 'id="btn-summary-refresh"' not in response.text
     assert '/static/js/shift_date.js?v=' in response.text
     assert '<details class="account-menu">' in response.text
+    assert (
+        'class="account-menu-item" href="/submission-archive">报送存档</a>'
+        in response.text
+    )
     assert 'class="account-menu-item" href="/admin">用户与排班</a>' in response.text
     assert 'href="/static/css/layout.css"' in response.text
     assert 'href="/static/css/modules/filter.css"' in response.text
